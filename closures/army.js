52b08819a731b94044547f76cf908ce72e1b01cc
Viacheslav <kovtunov.vyacheslav@gmail.com>
2019-09-15 20:38:45 +0300

    -a

---
+++ closures/army.js
@@ -0,0 +1,14 @@
+function makeArmy() {
+    let shooters = [];
+  
+    
+    for (let i = 0; i < 10; i++ ) {
+      let shooter = function() { // shooter function
+        alert( i ); // should show its number
+      };
+      shooters.push(shooter);
+      
+    }
+  
+    return shooters;
+  }
